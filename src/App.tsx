1521f64ac83b58330c8226d467b4bf344ebab81c
bunyaminfil <b.fil@extratik.com>
2022-02-09 20:11:28 +0300

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Button } from './components/Button';
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./containers/HomePage";
+import CheckoutPage from "./containers/CheckoutPage";
+import AllProductsPage from "./containers/AllProductsPage";
+import { ROUTE } from "./constants/route";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Primary</Button>
-      <Button>Default</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTE.HOME} element={<HomePage />} />
+          <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
+          <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
+          {/* <Navigate to={ROUTE.HOME} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
