a44d8dbb482431eefad3bffc7d512b7eb5f607f7
bunyaminfil <b.fil@extratik.com>
2022-02-09 20:31:29 +0300

    header navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import CheckoutPage from "./containers/CheckoutPage";
 import AllProductsPage from "./containers/AllProductsPage";
 import { ROUTE } from "./constants/route";
 import "./App.css";
+import { HeaderNavigation } from "./components/HeaderNavigation";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path={ROUTE.HOME} element={<HomePage />} />
-          <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
-          <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
-          {/* <Navigate to={ROUTE.HOME} /> */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+      <HeaderNavigation />
+      <Routes>
+        <Route path={ROUTE.HOME} element={<HomePage />} />
+        <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
+        <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
+        {/* <Navigate to={ROUTE.HOME} /> */}
+      </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
