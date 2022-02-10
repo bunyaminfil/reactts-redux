215bad03f14abd7a0c95376d10290656496b98ce
bunyaminfil <b.fil@extratik.com>
2022-02-10 18:42:25 +0300

    redux prepared

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
+import "./App.css";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./containers/HomePage";
 import CheckoutPage from "./containers/CheckoutPage";
 import AllProductsPage from "./containers/AllProductsPage";
 import { ROUTE } from "./constants/route";
-import "./App.css";
 import { HeaderNavigation } from "./components/HeaderNavigation";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./store/rootReducer";
+
+const store = createStore(rootReducer);
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-      <HeaderNavigation />
-      <Routes>
-        <Route path={ROUTE.HOME} element={<HomePage />} />
-        <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
-        <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
-        {/* <Navigate to={ROUTE.HOME} /> */}
-      </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="app-container">
+          <HeaderNavigation />
+          <Routes>
+            <Route path={ROUTE.HOME} element={<HomePage />} />
+            <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
+            <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
+            {/* <Navigate to={ROUTE.HOME} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
